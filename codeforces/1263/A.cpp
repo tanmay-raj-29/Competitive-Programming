b99853027d4dec9739afbf72dda4a53168f0dee5
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-05-19 00:53:00 +0530

    Add solution for problem `A - Sweet Problem` Link: https://codeforces.com/contest/1263/problem/A Tags: math, *1100 Ref: https://codeforces.com/contest/1263/submission/80634906

--- codeforces/1263/A.cpp
+++ codeforces/1263/A.cpp
@@ -1,87 +1,43 @@
-#include <bits/stdc++.h>
-using namespace std;
-void __print(int x) {cout << x;}
-void __print(long x) {cout << x;}
-void __print(long long x) {cout << x;}
-void __print(unsigned x) {cout << x;}
-void __print(unsigned long x) {cout << x;}
-void __print(unsigned long long x) {cout << x;}
-void __print(float x) {cout << x;}
-void __print(double x) {cout << x;}
-void __print(long double x) {cout << x;}
-void __print(char x) {cout << '\'' << x << '\'';}
-void __print(const char *x) {cout << '\"' << x << '\"';}
-void __print(const string &x) {cout << '\"' << x << '\"';}
-void __print(bool x) {cout << (x ? "true" : "false");}
-
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
-void _print() {cout << "]"<<endl;}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
-#define deb(x...) cout << #x << " = ["; _print(x)
-template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
-template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
-// =================
-#define f(i, k, n) for (int i = k; i < n; i++)
-#define r(i, k, n) for (int i = k; i >= n; i--)
-#define ll long long
+#include <bits/stdc++.h> 
 #define pb push_back
 #define fr first
 #define sc second
-#define len(s) s.size()
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
 #define all(v) v.begin(), v.end()
-#define tr(it, v) for (auto &it : v)
-typedef pair<int, int> pii;
+#define f(i,a,n) for(int i=a;i<n;i++)
+using namespace std;
 typedef vector<int> vi;
-typedef map<int, int> mii;
-typedef vector<pii> vpii;
+typedef map<int,int> mii;
+typedef pair<int,int> pii; 
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
 typedef vector<string> vs;
-typedef vector<vi> vvi;
-// =================
+const double pi=3.141592653589;
 
-const int mod = 1e9+7;
-const int N = 3e5 + 1;
+int main() {
 
-void solve()
-{
-    int n;
-    int r,g,b;
-    cin>>r>>g>>b;
-    int n1=max({r,g,b}),n3=min({r,g,b}),n2=r+g+b-n1-n3;
-    int days=0;
-    days+=(n2-n3);
-    n1-=days;
-    n2-=days;
-
-    // n1 n3 n3
-    if(n1>=n3*2) days+=n3*2;
-    else {
-        days+=(n1/2) * 2;
-        n3-=n1/2;
-        n2-=n1/2;
-        n1=(n1%2);
-       
-        days+=n2;
-    }
-    cout<<days<<"\n";
-}
-
-signed main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
+    #endif
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(NULL) ; cout.tie(NULL) ;
 
-    int t = 1;
-    cin>>t;
-    while (t--)
-        solve();
+    int t; cin>>t; while(t--) {
+        int r,g,b; cin>>r>>g>>b;
+        int mn=min3(r,g,b),mx=max3(r,g,b),mid=r+g+b-mx-mn;
+        if(mn+mid<=mx) cout<<mn+mid<<endl;
+        else {
+            int ans=mx-mid;
+            cout<<mn+min(mid-(mn-ans)/2,mid-(mn-ans+1)/2)<<endl;
+        }
+    }
+
     return 0;
 }
