37ef18029fcbe0874b35ab95af38182eb9592f76
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-05-19 00:15:00 +0530

    Add solution for problem `A - Changing Volume` Link: https://codeforces.com/contest/1255/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1255/submission/80632627

--- codeforces/1255/A.cpp
+++ codeforces/1255/A.cpp
@@ -1,75 +1,40 @@
-#include <bits/stdc++.h>
-using namespace std;
-void __print(int x) {cout << x;}
-void __print(long x) {cout << x;}
-void __print(long long x) {cout << x;}
-void __print(unsigned x) {cout << x;}
-void __print(unsigned long x) {cout << x;}
-void __print(unsigned long long x) {cout << x;}
-void __print(float x) {cout << x;}
-void __print(double x) {cout << x;}
-void __print(long double x) {cout << x;}
-void __print(char x) {cout << '\'' << x << '\'';}
-void __print(const char *x) {cout << '\"' << x << '\"';}
-void __print(const string &x) {cout << '\"' << x << '\"';}
-void __print(bool x) {cout << (x ? "true" : "false");}
-
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
-void _print() {cout << "]"<<endl;}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
-#define deb(x...) cout << #x << " = ["; _print(x)
-template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
-template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
-// =================
-#define f(i, k, n) for (int i = k; i < n; i++)
-#define r(i, k, n) for (int i = k; i >= n; i--)
-#define ll long long
+#include <bits/stdc++.h> 
 #define pb push_back
 #define fr first
 #define sc second
-#define len(s) s.size()
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
 #define all(v) v.begin(), v.end()
-#define tr(it, v) for (auto &it : v)
-typedef pair<int, int> pii;
+#define f(i,a,n) for(int i=a;i<n;i++)
+using namespace std;
 typedef vector<int> vi;
-typedef map<int, int> mii;
-typedef vector<pii> vpii;
+typedef map<int,int> mii;
+typedef pair<int,int> pii; 
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
 typedef vector<string> vs;
-typedef vector<vi> vvi;
-// =================
+const double pi=3.141592653589;
 
-const int mod = 1e9+7;
-const int N = 3e5 + 1;
+int main() {
 
-void solve()
-{
-    int a,b;
-    cin>>a>>b;
-    int c=abs(a-b);
-    int cnt=c/5;
-    c=c-(c/5)*5;
-    cnt+=c/2;
-    cnt+=c%2;
-    cout<<cnt<<"\n";
-}
-
-signed main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
+    #endif
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(NULL) ; cout.tie(NULL) ;
 
-    int t = 1;
-    cin>>t;
-    while (t--)
-        solve();
+    int t; cin>>t; while(t--) {
+        int a,b; cin>>a>>b;
+        int x=abs(a-b);
+        int cnt5=x/5,cnt2=(x-cnt5*5)/2,cnt1=x-cnt5*5-cnt2*2;
+        cout<<cnt5+cnt2+cnt1<<"\n";
+    }
+
     return 0;
 }
