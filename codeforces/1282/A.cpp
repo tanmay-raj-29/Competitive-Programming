a0a55479173d6dc53e43b8dfbac68020697a59c6
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-05-20 00:50:00 +0530

    Add solution for problem `A - Temporarily unavailable` Link: https://codeforces.com/contest/1282/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1282/submission/80721743

---
+++ codeforces/1282/A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h> 
+#define pb push_back
+#define fr first
+#define sc second
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define all(v) v.begin(), v.end()
+#define f(i,a,n) for(int i=a;i<n;i++)
+using namespace std;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+typedef pair<int,int> pii; 
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+const double pi=3.141592653589;
+
+int main() {
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL) ; cout.tie(NULL) ;
+
+    int t; cin>>t; while(t--) { 
+        int a,b,c,r; cin>>a>>b>>c>>r;
+        int mx=max(a,b), mn=min(a,b);
+        int dist=abs(b-a);
+        int l=max(mn,c-r), r0=min(mx,r+c);
+        cout<<dist- max(0,r0-l)<<endl;
+    }
+     
+    return 0;
+}
