01c4d6501aaa8cb47f847118dc657b084005c789
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-06-06 13:13:00 +0530

    Add solution for problem `A - Snacktower` Link: https://codeforces.com/contest/767/problem/A Tags: data structures, implementation, *1100 Ref: https://codeforces.com/contest/767/submission/82684887

---
+++ codeforces/767/A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define fr first
+#define sc second
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define all(v) v.begin(), v.end()
+#define f(i, a, n) for (int i = a; i < n; i++)
+using namespace std;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n; cin>>n;
+    int req=n;
+    priority_queue<int> q;
+    f(i,0,n) {
+        int x; cin>>x;
+        q.push(x);
+        while(!q.empty() && q.top()==req) {
+            cout<<q.top()<<" ";
+            q.pop();
+            req--;
+        }
+        cout<<endl;
+    }
+}
