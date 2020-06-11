95852f45c267e784fed88aef66ebd2a5973a24d7
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-06-11 13:13:00 +0530

    Add solution for problem `A - Cows and Primitive Roots` Link: https://codeforces.com/contest/284/problem/A Tags: implementation, math, number theory, *1400 Ref: https://codeforces.com/contest/284/submission/83359396

---
+++ codeforces/284/A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define fr first
+#define sc second
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define all(v) v.begin(), v.end()
+#define f(i, a, n) for (int i = a; i < n; i++)
+using namespace std;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int p;
+    cin >> p;
+    int i = 2, cnt = 0;
+    if(p==2) {
+        cout<<"1";
+        return 0;
+    }
+    while (i < p)
+    {
+        int ans=i,j=1;
+        while(j<p-1) {
+            if((ans-1)%p==0) break;
+            ans=(ans*i)%p;
+            j++;
+        }
+        if(j==p-1 && (ans-1)%p==0) cnt++; 
+        i++;
+    }
+    cout << cnt;
+    return 0;
+}
