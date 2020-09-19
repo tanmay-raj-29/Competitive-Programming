7d80c2bf5aff815ebfeebb60b16773c758e5de82
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-09-19 21:21:00 +0530

    Add solution for problem `D1 - Sage's Birthday (easy version)` Link: https://codeforces.com/contest/1419/problem/D1 Tags: binary search, constructive algorithms, greedy, sortings, *1000 Ref: https://codeforces.com/contest/1419/submission/93245669

--- codeforces/1419/D1.cpp
+++ codeforces/1419/D1.cpp
@@ -11,7 +11,7 @@ using namespace std;
 #define tr(it, v) for (auto &it : v)
 #define deb(x) cout << #x << "=" << x << endl
 #define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
-#define deb3(x, y, z) cout << #x << "=" << x << "," << #y << "=" << y << "," << #z << "=" << z << endl
+#define deb3(x, y, z) cout << #x << "=" << x << "," << #y << "=" << y << ","<< #z << "=" << z << endl
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef map<int, int> mii;
@@ -25,50 +25,25 @@ const int N = 3e5 + 1;
 
 void solve()
 {
-    int n;
-    cin >> n;
-    vi a(n);
-    tr(it, a) cin >> it;
-    sort(all(a));
-    int l = 0, r = n - 1, ans = 0;
-    while (l <= r)
-    {
-        int mid = l + (r - l) / 2;
-        int j = mid, cnt = a[mid - 1] < a[n - 1] ? 1 : 0;
-        if (!cnt)
-        {
-            r = mid - 1;
-            continue;
-        }
-        f(i, 0, mid)
-        {
-            int k = 0;
-            while (j < n - 1)
-            {
-                if (a[i] < a[j])
-                {
-                    cnt++;
-                    j++;
-                    break;
-                }
-                j++;
-            }
-        }
-        if (cnt > mid)
-            ans = mid, l = mid + 1;
-        else
-            r = mid - 1;
-        // deb3(mid,ans,cnt);
+    int n; cin>>n;
+    vi v(n);
+    tr(it,v) cin>>it;
+    sort(all(v));
+    cout<<(n-1)/2<<endl;
+    vi dp(n);
+    int j=0;
+    for(int i=1;i<n;i+=2) {
+        dp[i]=v[j];
+        j++;
     }
-    cout << ans << endl;
-    cout << a[n - 1] << " ";
-    int j = n - 2;
-    r(i, ans - 1, 0)
-    {
-        cout << a[i] << " " << a[j] << " ";
-        j--;
+    f(i,0,n) {
+        if(dp[i]==0) {
+            dp[i]=v[j];
+            j++;
+        }
     }
-    f(i, ans, j + 1) cout << a[j] << " ";
+    tr(it,dp) cout<<it<<" ";
+     
 }
 
 int main()
