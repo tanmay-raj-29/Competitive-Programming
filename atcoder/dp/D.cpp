1843cd90827eb500bc98a3d418b50fc00c8c2deb
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-10-06 06:51:00 +0530

    Add solution for problem `D - Knapsack 1` Link: https://atcoder.jp/contests/dp/tasks/dp_d Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/17215223

--- atcoder/dp/D.cpp
+++ atcoder/dp/D.cpp
@@ -1,53 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << '\'' << x << '\'';}
+void __print(const char *x) {cout << '\"' << x << '\"';}
+void __print(const string &x) {cout << '\"' << x << '\"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]"<<endl;}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+#define deb(x...) cout << #x << " = ["; _print(x)
 template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
 template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
 // =================
-#define f(i, k, n) for (int i = k; i < n; i++)
 #define ll long long
-#define int ll
-#define pb push_back
 #define fr first
 #define sc second
+#define pb push_back
 #define len(s) s.size()
 #define all(v) v.begin(), v.end()
 // =================
 
 const int mod = 1e9+7;
-const int N = 1e5 + 1;
-
-vector<vector<int>> dp(101,vector<int>(N,-1));
-
-int knapSack(int W, int wt[], int val[], int n) 
-{ 
-    if(n<=0 || W<=0) return 0;
-    if(dp[n][W]!=-1) return dp[n][W];
-    if(W<wt[n-1])
-    {
-        dp[n][W]=knapSack(W,wt,val,n-1);
-        return dp[n][W];
-    }
-    dp[n][W]=max(knapSack(W,wt,val,n-1),val[n-1]+knapSack(W-wt[n-1],wt,val,n-1));
-    return dp[n][W];
-}
+const int N = 3e5 + 1;
 
 void solve()
 {
-    int n,w;
-    cin>>n>>w;
-    int wt[n],val[n];
-    f(i,0,n) cin>>wt[i]>>val[i];
-    cout<<knapSack(w,wt,val,n);
+    int n,W; cin>>n>>W;
+    vector<int> v(n),w(n);
+    for(int i=0;i<n;i++) {
+        cin>>w[i]>>v[i];
+    }
+    vector<vector<ll>> dp(n,vector<ll>(W+1,-mod));
+    dp[0][0]=0;
+    dp[0][w[0]]=v[0];
+    for(int id=1;id<n;id++) {
+        dp[id][0]=0;
+        for(int j=1;j<=W;j++) {
+            dp[id][j]=dp[id-1][j];
+            if(j-w[id]>=0) amax(dp[id][j],dp[id-1][j-w[id]]+v[id]);
+        }
+    }
+    ll ans=0;
+    for(int i=0;i<=W;i++) amax(ans,dp[n-1][i]);
+    cout<<ans;
 }
-
-signed main()
+int main()
 {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int t=1;
-    // cin>>t;
+    int t = 1;
     while (t--)
         solve();
     return 0;
