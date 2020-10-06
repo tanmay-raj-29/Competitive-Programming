0daa21b74c08e9cf5ad5118b0aa53bd902e92f0e
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-10-06 09:31:00 +0530

    Add solution for problem `F - LCS` Link: https://atcoder.jp/contests/dp/tasks/dp_f Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/17216506

--- atcoder/dp/F.cpp
+++ atcoder/dp/F.cpp
@@ -1,13 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << '\'' << x << '\'';}
+void __print(const char *x) {cout << '\"' << x << '\"';}
+void __print(const string &x) {cout << '\"' << x << '\"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]"<<endl;}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+#define deb(x...) cout << #x << " = ["; _print(x)
 template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
 template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
 // =================
-#define f(i, k, n) for (int i = k; i < n; i++)
 #define ll long long
-#define pb push_back
 #define fr first
 #define sc second
+#define pb push_back
 #define len(s) s.size()
 #define all(v) v.begin(), v.end()
 // =================
@@ -17,40 +38,48 @@ const int N = 3e5 + 1;
 
 void solve()
 {
-    string a,b;
-    cin>>a>>b;
-    int n=len(a),m=len(b);
-    vector<vector<int>> dp(n+1,vector<int>(m+1)),choice(n+1,vector<int>(m+1));
-    f(i,1,n+1)
-    {
-        f(j,1,m+1)
-        {
-            if(a[i-1]==b[j-1]) dp[i][j]=dp[i-1][j-1]+1,choice[i][j]=2;            
-            else if(dp[i-1][j]>=dp[i][j-1]) dp[i][j]=dp[i-1][j],choice[i][j]=1;
-            else dp[i][j]=dp[i][j-1];
-        }
-    }
-    int i=n,j=m;
-    string ans="";
-    while(i&&j)
-    {
-        if(choice[i][j]==2) ans+=a[i-1],i--,j--;
-        else if(choice[i][j]==1) i--;
-        else j--;
-    }
-    reverse(all(ans));
-    cout<<ans;
+	string s,t; cin>>s>>t;
+    int n=len(s),m=len(t);
+    vector<vector<int>> dp(n,vector<int>(m,0));
+	vector<vector<int>> choice(n,vector<int>(m,0));
+	dp[0][0]=s[0]==t[0];
+	choice[0][0]= s[0]==t[0]+1;
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<m;j++) {
+			if(i) dp[i][j]=dp[i-1][j],choice[i][j]=0;
+			if(j && dp[i][j-1]>dp[i][j]) dp[i][j]=dp[i][j-1],choice[i][j]=1;
+			if(s[i]==t[j]) {
+				choice[i][j]=2;
+				int cnt=1;
+				if(i && j) cnt+=dp[i-1][j-1];
+				amax(dp[i][j],cnt);
+			}
+		}
+	}
+
+	int x=n-1,y=m-1;
+	string ans;
+	while (x>=0 && y>=0) {
+		if(choice[x][y]==0) x--;
+		else if(choice[x][y]==1) y--;
+		else ans.pb(s[x]),x--,y--;
+	}
+	reverse(all(ans));
+	cout<<ans;
 }
 
-signed main()
+int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int t=1;
-    // cin>>t;
-    while (t--)
-        solve();
-    return 0;
+	int t = 1;
+	while (t--)
+		solve();
+	return 0;
 }
