84c02e344ba33cdce0ffbb3617b3843532972506
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-10-13 16:00:00 +0530

    Add solution for problem `B - Towers` Link: https://codeforces.com/contest/479/problem/B Tags: brute force, constructive algorithms, greedy, implementation, sortings, *1400 Ref: https://codeforces.com/contest/479/submission/95413115

---
+++ codeforces/479/B.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << '\'' << x << '\'';}
+void __print(const char *x) {cout << '\"' << x << '\"';}
+void __print(const string &x) {cout << '\"' << x << '\"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]"<<endl;}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+#define deb(x...) cout << #x << " = ["; _print(x)
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+// =================
+#define f(i, k, n) for (int i = k; i < n; i++)
+#define r(i, k, n) for (int i = k; i >= n; i--)
+#define ll long long
+#define pb push_back
+#define fr first
+#define sc second
+#define len(s) s.size()
+#define all(v) v.begin(), v.end()
+#define tr(it, v) for (auto &it : v)
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<pii> vpii;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+// =================
+
+const int mod = 1e9+7;
+const int N = 3e5 + 1;
+
+void solve()
+{
+	int n,k;
+	cin>>n>>k;
+	vi a(n);
+	tr(it,a) cin>>it;
+	multiset<pii> ms;
+	f(i,0,n) ms.insert({a[i],i});
+	
+	vpii ans;
+	int opr=0;
+	while (opr<k)
+	{
+		pii low=*ms.begin();
+		auto it=ms.end();
+		it--;
+		pii high=*it;
+		if(high.fr==low.fr) break;
+		ms.erase(ms.begin());
+		ans.pb({high.sc,low.sc});
+		ms.erase(it);
+		ms.insert({low.fr+1,low.sc});
+		ms.insert({high.fr-1,high.sc});
+		opr++;
+	}
+	auto it=ms.end();
+	it--;
+	cout<<(*it).fr-(*ms.begin()).fr<<" "<<opr<<"\n";
+	tr(it,ans) cout<<it.fr+1<<" "<<it.sc+1<<"\n";
+}
+
+signed main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t = 1;
+	while (t--)
+		solve();
+	return 0;
+}
