8a64205eea1bbaa958fcbfab03db257c99c0bcb0
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-10-22 09:27:00 +0530

    Add solution for problem `G - Longest Path` Link: https://atcoder.jp/contests/dp/tasks/dp_g Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/17567281

---
+++ atcoder/dp/G.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << '\'' << x << '\'';}
+void __print(const char *x) {cout << '\"' << x << '\"';}
+void __print(const string &x) {cout << '\"' << x << '\"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]"<<endl;}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+#define deb(x...) cout << #x << " = ["; _print(x)
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+// =================
+#define f(i, k, n) for (int i = k; i < n; i++)
+#define r(i, k, n) for (int i = k; i >= n; i--)
+#define ll long long
+#define pb push_back
+#define fr first
+#define sc second
+#define len(s) s.size()
+#define all(v) v.begin(), v.end()
+#define tr(it, v) for (auto &it : v)
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<pii> vpii;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+// =================
+
+const int mod = 1e9+7;
+const int N = 1e5 + 1;
+vi dp(N);
+
+vector<int> g[N];
+int A[N];
+
+void impgraph(int n, int m)
+{
+    int i=0, u, v;
+    while (m--)
+    {
+        cin >> u >> v;
+        g[u].pb(v);
+    }
+}
+
+void dfs(int u, int par)
+{
+    A[u]=1;
+    dp[u]=par;
+    for (int v : g[u])
+    {
+        if (A[v])
+        {
+            amax(dp[u],dp[v]+1);
+            continue;
+        }
+        dfs(v,par);
+        amax(dp[u],dp[v]+1);
+        // deb(dp);
+    }
+}
+
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    impgraph(n,m);
+    f(i,1,n+1) {
+        if(!A[i]) dfs(i,0); 
+    }
+    int ans=0;
+    f(i,1,n+1) amax(ans,dp[i]);
+    cout<<ans;
+}
+
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    while (t--)
+        solve();
+    return 0;
+}
