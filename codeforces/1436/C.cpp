94206ca6fa79aa88098b25c3845d939efb3b0c7f
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-10-25 12:03:00 +0530

    Add solution for problem `C - Binary Search` Link: https://codeforces.com/contest/1436/problem/C Tags: binary search, combinatorics, *1500 Ref: https://codeforces.com/contest/1436/submission/96630802

--- codeforces/1436/C.cpp
+++ codeforces/1436/C.cpp
@@ -1,9 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << '\'' << x << '\'';}
+void __print(const char *x) {cout << '\"' << x << '\"';}
+void __print(const string &x) {cout << '\"' << x << '\"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]"<<endl;}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+#define deb(x...) cout << #x << " = ["; _print(x)
 template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
 template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
 // =================
 #define f(i, k, n) for (int i = k; i < n; i++)
+#define r(i, k, n) for (int i = k; i >= n; i--)
 #define ll long long
 #define int ll
 #define pb push_back
@@ -11,60 +34,66 @@ template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
 #define sc second
 #define len(s) s.size()
 #define all(v) v.begin(), v.end()
+#define tr(it, v) for (auto &it : v)
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<pii> vpii;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
 // =================
 
 const int mod = 1e9+7;
-const int N = 3e5 + 1;
+const int N = 3e3 + 1;
+const int MOD=mod;
 
-const int MYNUM = 3e5+1;
-const int MYMOD = mod;
-int fact[MYNUM], invfact[MYNUM];
+int fact[N], invfact[N];
 
 int pow(int a, int b, int m)
 {
-    int ans=1;
-    while(b)
-    {
-        if(b&1)
-            ans=(ans*a)%m;
-        b/=2;
-        a=(a*a)%m;
-    }
-    return ans;
+	int ans=1;
+	while(b)
+	{
+		if(b&1)
+			ans=(ans*a)%m;
+		b/=2;
+		a=(a*a)%m;
+	}
+	return ans;
 }
 
 int modinv(int k)
 {
-    return pow(k, MYMOD-2, MYMOD);
+	return pow(k, MOD-2, MOD);
 }
 
 void precompute()
 {
-    fact[0]=fact[1]=1;
-    for(int i=2;i<MYNUM;i++)
-    {
-        fact[i]=fact[i-1]*i;
-        fact[i]%=MYMOD;
-    }
-    invfact[MYNUM-1]=modinv(fact[MYNUM-1]);
-    for(int i=MYNUM-2;i>=0;i--)
-    {
-        invfact[i]=invfact[i+1]*(i+1);
-        invfact[i]%=MYMOD;
-    }
+	fact[0]=fact[1]=1;
+	for(int i=2;i<N;i++)
+	{
+		fact[i]=fact[i-1]*i;
+		fact[i]%=MOD;
+	}
+	invfact[N-1]=modinv(fact[N-1]);
+	for(int i=N-2;i>=0;i--)
+	{
+		invfact[i]=invfact[i+1]*(i+1);
+		invfact[i]%=MOD;
+	}
 }
 
 int nCr(int x, int y)
 {
-    if(y>x)
-        return 0;
-    int num=fact[x];
-    num*=invfact[y];
-    num%=MYMOD;
-    num*=invfact[x-y];
-    num%=MYMOD;
-    return num;
-} 
+	if(y>x)
+		return 0;
+	int num=fact[x];
+	num*=invfact[y];
+	num%=MOD;
+	num*=invfact[x-y];
+	num%=MOD;
+	return num;
+}
 
 void solve()
 {
@@ -81,6 +110,9 @@ void solve()
         else r=mid;        
     }
     int gt=n-x,ls=x-1;
+    // deb(b,s,gt,ls);
+    // f(i,0,10) cout<<fact[i]<<endl;
+    // cout<<(nCr(gt,b)+mod)%mod<<" "<<(nCr(ls,s)+mod)%mod<<endl;
     ll n1=nCr(gt,b),n2=nCr(ls,s),n3=fact[n-1-b-s];
     ll ans=1;
     ans*=n1;ans%=mod;
@@ -93,12 +125,15 @@ void solve()
 
 signed main()
 {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int t=1;
-    // cin>>t;
+    int t = 1;
     while (t--)
         solve();
     return 0;
