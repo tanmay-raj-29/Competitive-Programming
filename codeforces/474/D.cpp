c7267e64341779084ac8351028e3fb2e013858bf
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-10-27 17:10:00 +0530

    Add solution for problem `D - Flowers` Link: https://codeforces.com/contest/474/problem/D Tags: dp, *1700 Ref: https://codeforces.com/contest/474/submission/96852060

---
+++ codeforces/474/D.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << '\'' << x << '\'';}
+void __print(const char *x) {cout << '\"' << x << '\"';}
+void __print(const string &x) {cout << '\"' << x << '\"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]"<<endl;}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+#define deb(x...) cout << #x << " = ["; _print(x)
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+// =================
+#define f(i, k, n) for (int i = k; i < n; i++)
+#define r(i, k, n) for (int i = k; i >= n; i--)
+#define ll long long
+#define int ll
+#define pb push_back
+#define fr first
+#define sc second
+#define len(s) s.size()
+#define all(v) v.begin(), v.end()
+#define tr(it, v) for (auto &it : v)
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<pii> vpii;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+// =================
+
+const int mod = 1e9+7;
+const int N = 1e5+1;
+int k;
+vi dp(N);
+// dp[i] number of ways to eat i flowers
+
+void compute() {
+    dp[0]=1;
+    f(i,1,N) {
+        dp[i]+=dp[i-1];
+        if(i>=k) dp[i]+=dp[i-k];
+        dp[i]%=mod;
+    }
+    dp[0]=0;
+    f(i,1,N) {
+        dp[i]=dp[i]+dp[i-1];
+        dp[i]%=mod;
+    }
+}
+
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<(dp[b]-dp[a-1]+mod)%mod<<"\n";
+}
+
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin>>t>>k;
+    compute();
+    while (t--)
+        solve();
+    cerr<<(1000*clock())/CLOCKS_PER_SEC<<"ms";
+    return 0;
+}
