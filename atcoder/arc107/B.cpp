80f341907e911a36fd4b923e59eb1e709f381f3b
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-10-31 18:32:00 +0530

    Add solution for problem `B - Quadruple` Link: https://atcoder.jp/contests/arc107/tasks/arc107_b Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/arc107/submissions/17758622

---
+++ atcoder/arc107/B.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << '\'' << x << '\'';}
+void __print(const char *x) {cout << '\"' << x << '\"';}
+void __print(const string &x) {cout << '\"' << x << '\"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]"<<endl;}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+#define deb(x...) cout << #x << " = ["; _print(x)
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+// =================
+#define f(i, k, n) for (int i = k; i < n; i++)
+#define r(i, k, n) for (int i = k; i >= n; i--)
+#define ll long long
+// #define int ll
+#define pb push_back
+#define fr first
+#define sc second
+#define len(s) s.size()
+#define all(v) v.begin(), v.end()
+#define tr(it, v) for (auto &it : v)
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<pii> vpii;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+// =================
+
+const int mod = 1e9+7;
+const int N = 3e5 + 1;
+
+void solve()
+{
+    int n,k;
+    cin>>n>>k;
+    ll cnt=0;
+    int sum=max(k+2,2);
+    while (sum<=2*n)
+    {
+        int req=sum-k;
+        int r1=min(n,sum-1),l1=max(sum-r1,1);
+        int r2=min(n,req-1) , l2= max(req-r2,1);
+        // deb(sum,req);
+        sum++;
+        // deb(l1,r1,l2,r2);
+        if(max(l1,r1)>n || max(l2,r2)>n || min(l1,r1)<1 || min(l2,r2)<1) continue;
+        cnt+= 1ll*(abs(r1-l1) + 1)*(abs(r2- l2) + 1);
+        // deb(cnt);
+    }
+    
+    cout<<cnt;
+}
+
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    while (t--)
+        solve();
+    // cerr<<(1000*clock())/CLOCKS_PER_SEC<<"ms";
+    return 0;
+}
