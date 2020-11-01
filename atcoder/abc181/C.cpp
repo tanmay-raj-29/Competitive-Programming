fa7d1226ea81892b87d115ac297f4c5397336e10
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-11-01 17:38:00 +0530

    Add solution for problem `C - Collinearity` Link: https://atcoder.jp/contests/abc181/tasks/abc181_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc181/submissions/17789421

---
+++ atcoder/abc181/C.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << '\'' << x << '\'';}
+void __print(const char *x) {cout << '\"' << x << '\"';}
+void __print(const string &x) {cout << '\"' << x << '\"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]"<<endl;}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+#define deb(x...) cout << #x << " = ["; _print(x)
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+// =================
+#define f(i, k, n) for (int i = k; i < n; i++)
+#define r(i, k, n) for (int i = k; i >= n; i--)
+#define ll long long
+#define int ll
+#define pb push_back
+#define fr first
+#define sc second
+#define len(s) s.size()
+#define all(v) v.begin(), v.end()
+#define tr(it, v) for (auto &it : v)
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<pii> vpii;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+// =================
+
+const int mod = 1e9+7;
+const int N = 3e5 + 1;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vpii v;
+    f(i,0,n) {
+        int x,y;
+        cin>>x>>y;
+        v.pb({x,y});
+    }
+    f(i,0,n) {
+        f(j,i+1,n) {
+            f(k,j+1,n) {
+                if( (v[k].sc-v[j].sc)*(v[j].fr- v[i].fr) == (v[j].sc-v[i].sc)*(v[k].fr- v[j].fr) ) {
+                    cout<<"Yes\n";
+                    return;
+                }
+            }
+        }
+    }
+    cout<<"No\n";
+}
+
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    while (t--)
+        solve();
+    return 0;
+}
