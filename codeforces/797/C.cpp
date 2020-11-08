a1104ee2a5f54316d54e41eabfd09c60d9cf36d2
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-11-08 12:14:00 +0530

    Add solution for problem `C - Minimal string` Link: https://codeforces.com/contest/797/problem/C Tags: data structures, greedy, strings, *1700 Ref: https://codeforces.com/contest/797/submission/97897137

---
+++ codeforces/797/C.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << '\'' << x << '\'';}
+void __print(const char *x) {cout << '\"' << x << '\"';}
+void __print(const string &x) {cout << '\"' << x << '\"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]"<<endl;}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+#define deb(x...) cout << #x << " = ["; _print(x)
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+// =================
+#define f(i, k, n) for (int i = k; i < n; i++)
+#define r(i, k, n) for (int i = k; i >= n; i--)
+#define ll long long
+#define pb push_back
+#define fr first
+#define sc second
+#define len(s) s.size()
+#define all(v) v.begin(), v.end()
+#define tr(it, v) for (auto &it : v)
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+typedef vector<pii> vpii;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+// =================
+
+const int mod = 1e9+7;
+const int N = 3e5 + 1;
+
+void solve()
+{
+    string s;
+    cin>>s;
+    int n=len(s);
+    vi aux;
+    f(i,0,n) {
+        char c=s[i];
+        while (!aux.empty() && s[aux.back()]>c)
+        {
+            aux.pop_back();
+        }
+        aux.pb(i);
+    }
+
+    int curr=0,i=0,prev=-1;
+    vi rem;
+    while (curr<len(aux)) {
+        while (!rem.empty() && s[rem.back()]<=s[aux[curr]])
+        {
+            cout<<s[rem.back()];
+            rem.pop_back();
+        }
+        cout<<s[aux[curr]];
+        f(j,prev+1,aux[curr]) rem.pb(j);
+        prev=aux[curr];
+        curr++;
+        i++;
+    }
+    while (!rem.empty())
+    {
+        cout<<s[rem.back()];
+        rem.pop_back();
+    }
+    
+    // r(i,n-1,0) if(s[i]!='#') cout<<s[i];
+}
+
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    while (t--)
+        solve();
+    return 0;
+}
