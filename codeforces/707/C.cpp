42997807347a5cec21fdde8db278254382baf790
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-12-01 11:59:00 +0530

    Add solution for problem `C - Pythagorean Triples` Link: https://codeforces.com/contest/707/problem/C Tags: math, number theory, *1500 Ref: https://codeforces.com/contest/707/submission/100086117

---
+++ codeforces/707/C.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+// =================
+#define f(i, k, n) for (int i = k; i < n; i++)
+#define ll long long
+#define pb push_back
+#define fr first
+#define sc second
+#define len(s) s.size()
+#define all(v) v.begin(), v.end()
+// =================
+
+const int mod = 1e9+7;
+const int N = 3e5 + 1;
+
+void solve()
+{
+    int a;
+    cin>>a;
+    if(a<3) cout<<"-1";
+    else if(a%2) cout<<(1ll*a*a+1)/2<<" "<<(1ll*a*a-1)/2;
+    else cout<<1ll*a*a/4+1<<" "<<1ll*a*a/4-1;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t=1;
+    // cin>>t;
+    while (t--)
+        solve();
+    return 0;
+}
