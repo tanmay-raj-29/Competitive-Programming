97b469b79ecc101e45097e5f12ae7281bfdccf91
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-12-01 16:09:00 +0530

    Add solution for problem `B - Reverse Binary Strings` Link: https://codeforces.com/contest/1437/problem/B Tags: constructive algorithms, greedy, *1200 Ref: https://codeforces.com/contest/1437/submission/100103130

---
+++ codeforces/1437/B.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+// =================
+#define f(i, k, n) for (int i = k; i < n; i++)
+#define ll long long
+#define pb push_back
+#define fr first
+#define sc second
+#define len(s) s.size()
+#define all(v) v.begin(), v.end()
+// =================
+
+const int mod = 1e9+7;
+const int N = 3e5 + 1;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int cnt0=0,cnt1=0;
+    f(i,1,n)
+    {
+        if(s[i]==s[i-1]) 
+        {
+            cnt0+=s[i]=='0';
+            cnt1+=s[i]=='1';
+        }
+    }
+    int ans=max(cnt0,cnt1);
+    cout<<ans<<"\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t=1;
+    cin>>t;
+    while (t--)
+        solve();
+    return 0;
+}
