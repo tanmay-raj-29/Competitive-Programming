30c2554fd28d9ae81cd97a0dd355a725ca4d87f2
Tanmay Raj <tanmayraj29.20@gmail.com>
2020-12-19 17:31:00 +0530

    Add solution for problem `A - Brick` Link: https://atcoder.jp/contests/abc186/tasks/abc186_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc186/submissions/18861994

---
+++ atcoder/abc186/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+// =================
+#define f(i, k, n) for (int i = k; i < n; i++)
+#define ll long long
+#define pb push_back
+#define fr first
+#define sc second
+#define len(s) s.size()
+#define all(v) v.begin(), v.end()
+// =================
+
+const int mod = 1e9+7;
+const int N = 3e5 + 1;
+
+void solve()
+{
+    int n,w;
+    cin>>n>>w;
+    cout<<n/w;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t=1;
+    // cin>>t;
+    while (t--)
+        solve();
+    return 0;
+}
